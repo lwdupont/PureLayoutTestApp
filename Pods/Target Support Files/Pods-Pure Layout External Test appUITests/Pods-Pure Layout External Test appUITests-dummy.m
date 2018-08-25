fec8c56aa30a4551c96c4b39059161f4f2afdd1b
Lucien Dupont <lucien@chromedomesoftware.com>
2018-08-24 21:45:59 -0700

    Added PureLayout as a CocoaPod.

---
+++ Pods/Target Support Files/Pods-Pure Layout External Test appUITests/Pods-Pure Layout External Test appUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pure_Layout_External_Test_appUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Pure_Layout_External_Test_appUITests
+@end
