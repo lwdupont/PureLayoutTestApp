2c096483984444b932138aad1e3e9a1bbb63f5ab
Lucien Dupont <lucien@chromedomesoftware.com>
2018-08-24 21:38:40 -0700

    Initial check in

---
+++ Pure Layout External Test appTests/Pure_Layout_External_Test_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pure_Layout_External_Test_appTests.swift
+//  Pure Layout External Test appTests
+//
+//  Created by Lucien Dupont on 8/24/18.
+//  Copyright © 2018 Chromedome Software. All rights reserved.
+//
+
+import XCTest
+@testable import Pure_Layout_External_Test_app
+
+class Pure_Layout_External_Test_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
