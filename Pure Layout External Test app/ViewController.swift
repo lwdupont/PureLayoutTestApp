56e1ab88d6f58246792991c864f325b13a24bf93
Lucien Dupont <lucien@chromedomesoftware.com>
2018-08-25 13:18:06 -0700

    Quick test to make sure the pod works in a 'real' app.

--- Pure Layout External Test app/ViewController.swift
+++ Pure Layout External Test app/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import PureLayout
 
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		quickTestOfPureLayout()
+	}
+	
+	func quickTestOfPureLayout() {
+		
+		let uiView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+		uiView.backgroundColor = UIColor.purple
+		view.addSubview(uiView)
+		
+		uiView.autoPinEdgesToSuperviewMargins()
 	}
 
 	override func didReceiveMemoryWarning() {
